54b465c1649f90ca8718c515e4487cfd1cad8406
Mohit Singh Solanki <112701441+MSinghSolanki@users.noreply.github.com>
2023-11-06 17:24:55 +0530

    NEW

--- board/src/components/Board/Board.js
+++ board/src/components/Board/Board.js
@@ -4,172 +4,97 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import './Board.css';
+import { toast } from 'react-toastify';
 
 const TaskBoard = () => {
+  const BSE_URL = process.env.REACT_APP_BASE_URL;
   let data = {
     task_title: "",
     task_description: "",
     listing_id: "",
     list_title: ""
   }
+
   const [lists, setLists] = useState([]);
-  const [listTitle, setListTitle] = useState('');
-  const [showNewListInput, setShowNewListInput] = useState(false);
   const [values, setValues] = useState(data);
-  const [listCreated, setListCreated] = useState(false)
-  const [lastListingId, setLastListingId] = useState(0);
-  const [taskValues, setTaskValues] = useState([]);
+  const [showNewListInput, setShowNewListInput] = useState(false);
+
 
   const toggleNewListInput = () => {
     setShowNewListInput(!showNewListInput);
   };
 
- 
   const handleListCreate = () => {
-    if (listTitle) {
-      const newList = { listing_id: lastListingId+1, list_title: listTitle, tasks: [] };
-      setLists([...lists, newList]);
-      localStorage.setItem('lists', JSON.stringify([...lists, newList]));
+    if (values?.list_title === "") {
+      alert("Please Enter New List Title")
+    } else {
+      setShowNewListInput(false);
+      setLists([...lists, {
+        "listing_id": "",
+        "list_title": values?.list_title,
+        "tasks": [
+        ]
+      }])
 
-      // Set listCreated to true when a new list is created
-      setListCreated(true);
     }
-    setListTitle('');
-    setShowNewListInput(false);
-  };
+  }
 
-  const setTaskValuesForList = (listIndex, newValues) => {
-    const updatedValues = [...taskValues];
-    updatedValues[listIndex] = newValues;
-    setTaskValues(updatedValues);
-  };
-    // Input Handling for a specific list
-    const handleInputForList = (e, listIndex) => {
-      const { name, value } = e.target;
-      const newValues = { ...taskValues[listIndex], [name]: value };
-      setTaskValuesForList(listIndex, newValues);
-    };
+  const handleInputForList = (e) => {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value })
+    console.log("values", values)
+  }
 
-    const handleCreateTask = async (listIndex) => {
-      if (listIndex >= 0) {
-        const newTask = {
-          task_title: taskValues[listIndex]?.task_title,
-          task_description: taskValues[listIndex]?.task_description,
-        };
-    
-        let dataToSend = {};
-        const userToken = localStorage.getItem('tokens');
-        const config = {
-          headers: { Authorization: `Bearer ${userToken}` },
-        };
-    
-        // Fetch the listing_id from the API
-        const response = await axios.get('http://localhost:3002/api/task/listing', config);
-        const listingId = response?.data.data[0]?.listing_id
-     
-    
-         // Assuming it's the same for all items
-        if (listCreated && lists[listIndex].tasks.length == 0 || listingId==undefined) {
-          dataToSend = {
-            listing_id:"",
-            list_title:lists[listIndex].list_title,
-            task_title:newTask.task_title,
-            task_description:newTask.task_description,
-          };
-        } else if (lists[listIndex].listing_id == listingId) { 
-      ;
-          console.log("length 1 data :",response.data.data[0].listing_id)
-          dataToSend = {
-            task_title:newTask.task_title,
-            task_description:newTask.task_description,
-            listing_id:listingId,
-            list_title:""
-          };
-        }
-    
-        try {
-          await sendTaskData(dataToSend);
-          await handleGetData();
-    
-          const newTaskValues = {
-            ...taskValues[listIndex],
-            task_title: '',
-            task_description: '',
-          };
-          setTaskValuesForList(listIndex, newTaskValues);
-    
-          const updatedLists = [...lists];
-          updatedLists[listIndex].tasks.push(newTask);
-          setLists(updatedLists);
-        } catch (error) {
-          console.error('Error sending data to API:', error);
-        }
-      }
-    };
-    
-    
-    
-  const sendTaskData = (data) => {
-    console.log("data",data)
+  const sendTaskData = (id) => {
     const userToken = localStorage.getItem('tokens');
-    const config = {
-      headers: { Authorization: `Bearer ${userToken}` },
-    };
-    
-    axios.post('http://localhost:3002/api/task/create', data, config)
-   
+    const config = { headers: { Authorization: `Bearer ${userToken}` }, }
+
+    let sendTaskData
+    if (id == "" && values?.list_title != "") {
+      sendTaskData = {
+        task_title: values?.task_title,
+        task_description: values?.task_description,
+        listing_id: "",
+        list_title: values?.list_title
+      }
+    } else {
+      sendTaskData = {
+        task_title: values?.task_title,
+        task_description: values?.task_description,
+        listing_id: id,
+        list_title: ""
+      }
+    }
+
+    axios.post(BSE_URL + '/api/task/create', sendTaskData, config)
       .then((response) => {
         console.log('Data sent to API:', response);
-        // You can handle any further actions or updates here
+        setValues(data)
+        handleGetData();
+        setClearStatus(true)
       })
       .catch((error) => {
         console.error('Error sending data to API:', error);
       });
-    
-  };
+  }
 
-  const handleGetData = async () => {
+  const handleGetData = () => {
     const userToken = localStorage.getItem('tokens');
-    const config = {
-      headers: { Authorization: `Bearer ${userToken}` },
-    };
-  
-    try {
-      const response = await axios.get('http://localhost:3002/api/task/listing', config);
-  
-      if (response.data.status === "success") {
-        const responseData = response.data.data;
-        const mappedData = responseData.map((item) => ({
-          listing_id: item.listing_id,
-          list_title: item.list_title,
-          tasks: item.tasks,
-        }));
-        setLists(mappedData);
-  
-        setLists(response.data.data);
-      } else {
-        console.error('Data structure is not as expected.');
-      }
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-   
-  };
+    const config = { headers: { Authorization: `Bearer ${userToken}` }, }
 
-  
-
-
+    axios.get(`${BSE_URL}/api/task/listing`, config)
+      .then((response) => {
+        if (response?.data?.status === "success") {
+          setLists(response?.data?.data);
+        } else {
+          console.error('Data structure is not as expected.');
+        }
+      })
+  }
 
   useEffect(() => {
     handleGetData();
   }, []);
-
-  const handleInput = (e) => {
-    const { name, value } = e.target;
-    setValues({ ...values, [name]: value });
-  };
-
- 
 
   return (
     <div className="task-board">
@@ -184,44 +109,49 @@ const TaskBoard = () => {
               name="list_title"
               type="text"
               placeholder="New List Title"
-              value={listTitle}
-              onChange={(e) => setListTitle(e.target.value)}
+              // value={listTitle}
+              onChange={handleInputForList}
             />
             <button onClick={handleListCreate}>Create List</button>
           </div>
         )}
       </div>
       <div className="lists-container">
-      {lists.map((list, listIndex) => (
-    <div key={listIndex} className="list">
-      <h3>{list.list_title}</h3>
-      <ul>
-        {list.tasks.map((task, taskIndex) => (
-          <li key={taskIndex}>
-            <div>{task.task_title}</div>
-            <div>{task.task_description}</div>
-          </li>
+        {lists?.map((item, listIndex) => (
+          <div key={listIndex} className="list">
+            <h3>{item?.list_title}</h3>
+            <ul>
+              {item?.tasks.map((taskData, taskIndex) => (
+                <li key={taskIndex}>
+                  <div>{taskData?.task_title}</div>
+                  <div>{taskData?.task_description}</div>
+                </li>
+              ))}
+            </ul>
+
+            <div className="task-creation">
+              <input
+                name="task_title"
+                type="text"
+                placeholder="New Task Title"
+                // value={values?.task_title}
+                onChange={handleInputForList}
+              />
+              <input
+                name="task_description"
+                type="text"
+                placeholder="Task Description"
+                onChange={handleInputForList}
+              />
+
+            </div>
+
+            <button onClick={(e) => sendTaskData(item?.listing_id)}>Add Task</button>
+
+
+
+          </div>
         ))}
-      </ul>
-      <div className="task-creation">
-        <input
-          name="task_title"
-          type="text"
-          placeholder="New Task Title"
-          value={taskValues[listIndex]?.task_title || ''}
-          onChange={(e) => handleInputForList(e, listIndex)}
-        />
-        <input
-          name="task_description"
-          type="text"
-          placeholder="Task Description"
-          value={taskValues[listIndex]?.task_description || ''}
-          onChange={(e) => handleInputForList(e, listIndex)}
-        />
-        <button onClick={() => handleCreateTask(listIndex)}>Add Task</button>
-      </div>
-    </div>
-  ))}
 
       </div>
     </div>
